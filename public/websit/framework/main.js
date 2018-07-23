c1f191d005c3ede991f315c64cc616e1a5710acb
ccfromstar <114413511@qq.com>
2018-07-23 08:05:06 +0800

    update

--- public/websit/framework/main.js
+++ public/websit/framework/main.js
@@ -7,7 +7,7 @@ $(function(){
 	txtHeader += '<div class="cw-icon"><i class="ci-count" id="shopping-amount">-</i><img onclick="referToCart()" style="height:25px;margin-left:10px;margin-top:-5px;cursor:pointer;" src="../framework/cart.png" /></div>';
 	
 	}else{
-		txtHeader += '<span style="margin-left:20px;cursor:pointer;" onclick="gotologin()">登录</span> | <span style="cursor:pointer;" onclick="gotoreg()">注册</span> ';
+		txtHeader += '<span style="margin-left:20px;display:inline-block;cursor:pointer;margin-top:3px" onclick="gotologin()">登录</span> | <span style="cursor:pointer;" onclick="gotoreg()">注册</span> ';
 	
 	}
 	txtHeader += '</div>';
@@ -19,7 +19,7 @@ $(function(){
 	txtMenu += '<div style="float:left;margin-top:60px;margin-left:20px">';
 	txtMenu += '<span class="menu_span" id="m1" style="cursor:pointer;" onclick="referTo(0)">首页</span> | <span class="menu_span" id="m2" style="cursor:pointer;" onclick="referTo(1)">关于我们</span> | <span class="menu_span" id="m3" style="cursor:pointer;" onclick="referTo(2)">主营业务</span> | <span class="menu_span" id="m4" style="cursor:pointer;" onclick="referTo(3)">在线商城</span>';
 	txtMenu += '</div>';
-	txtMenu += '<div style="float:right;margin-top:60px;"><input id="pname" type="text" style="width:200px" placeholder=" 搜索商品"></input><img onclick="searchPro()" style="cursor:pointer;height:30px;margin-left:10px" src="../framework/search.png" /></div>';
+	txtMenu += '<div style="float:right;margin-top:56px;"><input id="pname" type="text" style="width:200px" placeholder=" 搜索商品"></input><img onclick="searchPro()" style="cursor:pointer;height:30px;margin-left:10px" src="../framework/search.png" /></div>';
 	txtMenu += '<div style="clear:both"></div>';
 	txtMenu += '</div>';
 	$("#menu").html(txtMenu);
@@ -27,12 +27,12 @@ $(function(){
 	var txtFooter = '';
 	txtFooter += '<div class="w960">';
 	txtFooter += '<div style="float:left;">';
-	txtFooter += '<dl class="fdl"><dt style="margin-top:19px" onclick="referTo(0)">首页</dt><dd></dd></dl>';
+	txtFooter += '<dl class="fdl"><dt class="fdll1" style="margin-top:19px" onclick="referTo(0)">首页</dt><dd></dd></dl>';
 	txtFooter += '<dl class="fdl"><dt>关于我们</dt><dd onclick="referTo(1)">公司简介</dd><dd onclick="referTo(11)">最新消息</dd><dd onclick="referTo(12)">联系我们</dd></dl>';
 	txtFooter += '<dl class="fdl"><dt>主营业务</dt><dd onclick="referTo(2)">自主售货机</dd><dd onclick="referTo(21)">预包装食品</dd><dd onclick="referTo(22)">电子设备</dd></dl>';
 	txtFooter += '<dl class="fdl"><dt>在线商城</dt><dd onclick="referTo(3)">饮料系列</dd><dd onclick="referTo(3)">零食系列</dd><dd onclick="referTo(3)">甜点系列</dd><dd onclick="referTo(3)">冰淇凌系列</dd></dl>';
 	txtFooter += '</div>';
-	txtFooter += '<div style="float:left;margin-top:20px;margin-left:20px">';
+	txtFooter += '<div style="float:left;margin-top:16px;margin-left:20px">';
 	txtFooter += '<div class="footer_1">客服热线</div>';
 	txtFooter += '<div class="footer_2">400-1223-2133</div>';
 	txtFooter += '<div class="footer_3">工作时间：周一至周五9:00-18:00</div>';
@@ -43,6 +43,7 @@ $(function(){
 	txtFooter += '<div class="w960" style="text-align:center;margin-top:80px;padding-bottom:10px">沪ICP证130164号<span style="margin-left:30px">Copyright ©</span> 2018 AIYIN All Rights Reserved.';
 	txtFooter += '</div>';
 	txtFooter += '<div id="logink" style="display:none;width:400px;height: 300px;border-radius: 20px;background: white;box-shadow:0px 0px 15px #888888; position: fixed;top:30%;left:40%;padding: 80px;">';
+	txtFooter += '			<div style="margin-left:280px;margin-top:-70px;cursor:pointer;" onclick="closeWin()"><img src="../framework/cha.png"></div><br>';
 	txtFooter += '			<div style="margin-left:90px;margin-top:-30px;">账号登陆</div><br>';
 	txtFooter += '			<div><input type="text" id="mobile" style="width:240px;height: 32px;" name="" placeholder="手机号码"></div><br>';
 	txtFooter += '			<div><input type="password" id="pwd" style="width:240px;height: 32px;" name="" placeholder="登陆密码"></div>';
@@ -79,6 +80,30 @@ $(function(){
 	}
 	getCartNum();
 });
+
+function closeWin(){
+	$("#logink").css("display","none");
+}
+
+function setH(i){
+	$("#HH1").removeClass("active1");
+	$("#HH2").removeClass("active1");
+	$("#HH3").removeClass("active1");
+	$("#HH"+i).addClass("active1");
+	if(i == 1){
+		$("#IMG1").attr("src","../framework/a1.jpg");
+		$("#IMG2").attr("src","../framework/a2.jpg");
+		$("#IMG3").attr("src","../framework/a3.jpg");
+	}else if(i==2){
+		$("#IMG1").attr("src","../framework/b1.jpg");
+		$("#IMG2").attr("src","../framework/b2.jpg");
+		$("#IMG3").attr("src","../framework/b3.jpg");
+	}else if(i==3){
+		$("#IMG1").attr("src","../framework/c1.jpg");
+		$("#IMG2").attr("src","../framework/c2.jpg");
+		$("#IMG3").attr("src","../framework/c3.jpg");
+	}
+}
 
 function searchPro(){
 	sessionStorage.pname = $("#pname").val();
